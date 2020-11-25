9933d9dbe59bafaeee555671be9ce358ec4c30d9
TheHelias <akande.tolu@yahoo.com>
2020-11-25 15:41:12 +0100

    test: write sign up test

--- src/test/tests.js
+++ src/test/tests.js
@@ -1,4 +1,4 @@
-import User from '../model/users'
+import User from '../model/user'
 
 const chai = require('chai')
 const chaiHttp = require('chai-http')
@@ -9,3 +9,24 @@ const should = chai.should()
 const { expect } = chai
 chai.use(assertArrays)
 chai.use(chaiHttp)
+
+const newUser = {
+  name: 'John Gabe',
+  email: 'jg@hey.com',
+  password: 'TestingWithChai',
+  phone_no: '08043434343'
+}
+const user = new User(newUser)
+
+describe('/Create User', () => {
+  it('it should create a user', (done) => {
+    chai.request(server)
+      .post('/api/user/create')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(201)
+        expect(res.body).to.deep.equal({ message: 'Congrats! You have successfully signed up with Exambly!' })
+        done()
+      })
+  })
+})
